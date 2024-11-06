fb221ccf944be8363a3c5d61ce5af554a9fbb1c8
Catherine Nduati <wairimucatherine.ke@gmail.com>
2024-11-06 18:30:40 +0300

    Added alx-react-app-props directory

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -8,8 +8,8 @@ import Header from './components/Header'
 import MainContent from './components/MainContent'
 import Footer from './components/Footer'
 import UserProfile from './components/UserProfile'
-import ProfilePage from './ProfilePage';
-import UserContext from './UserContext';
+import ProfilePage from './components/ProfilePage';
+import UserContext from './components/UserContext';
 
 function App() {
   const [count, setCount] = useState(0)
