22be3ec9e447f7c8c576d2368580e71ee8b3462b
Catherine Nduati <wairimucatherine.ke@gmail.com>
2024-11-26 14:53:37 +0300

    Implement React Query data handling

---
+++ react-query-demo/src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import React from "react";
+import { QueryClient, QueryClientProvider } from "react-query";
+import PostsComponent from "./components/PostsComponent";
+
+const queryClient = new QueryClient();
+
+  function App() {
+    return (
+      <QueryClientProvider client={queryClient}>
+        <PostsComponent />
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
+    );
+}
+
+export default App
