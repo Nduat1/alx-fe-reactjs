54aa1ee4ec6e7c90e3e7bd534c5188220e5777e0
Catherine Nduati <wairimucatherine.ke@gmail.com>
2024-11-27 13:20:41 +0300

    Deleted the form-handling-react directory

--- alx-react-app/alx-react-app/src/App.jsx
+++ alx-react-app/alx-react-app/src/App.jsx
@@ -2,16 +2,12 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import React from "react";
-import RegistrationForm from "./components/RegistrationForm";
-
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-    <RegistrationForm />
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
