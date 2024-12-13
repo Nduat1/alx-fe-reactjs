5ca67cac4b415a9e994b619d26d1a98079a9b97f
Catherine Nduati <wairimucatherine.ke@gmail.com>
2024-12-13 09:58:32 +0300

    Added github-user-search directory

---
+++ github-user-search/src/App.jsx
@@ -0,0 +1,17 @@
+import React from "react"
+import { Route, RouterProvider, Routes } from "react-router-dom"
+
+function App() {
+  
+  return (
+    <>
+      <Route>
+        <Routes>
+          <RouterProvider></RouterProvider>
+        </Routes>
+      </Route>
+    </>
+  )
+}
+
+export default App
